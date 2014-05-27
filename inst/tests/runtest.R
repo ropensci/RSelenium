1c1e7365e04bb9a4626e0c7ebbc283f94f09bc41
johndharrison <johndharrison0@gmail.com>
2014-05-27 23:42:54 +0700

    Add multi OS/Browser support for package testing.

--- inst/tests/runtest.R
+++ inst/tests/runtest.R
@@ -1,25 +1,43 @@
+require(testthat)
 user <- "rselenium0"
 pass <- "49953c74-5c46-4ff9-b584-cf31a4c71809"
 port <- 80
+selVersion <- "2.42.0"
 ip <- paste0(user, ':', pass, "@ondemand.saucelabs.com")
-#browser <- "safari"
-#browser <- "firefox"
-#browser <- "chrome"
-#browser <- "internet explorer"
-#browser <- "android"
-browser <- "safari"
-version <- "7"
-#platform <- "Windows 8.1"
-# platform <- "linux"
-platform <- "OS X 10.9"
+testDir <- system.file("tests", package = "RSelenium")
+osBrowser <- list(
+  "OS X 10.9" = list(list(browser = "safari", version = '7')
+                     , list(browser = "firefox", version = '28')
+                     , list(browser = "chrome", version = '34')
+  ),
+  "Windows 8" = list(list(browser = "chrome", version = '34')
+                     , list(browser = "firefox", version = '29')
+                     , list(browser = "internet explorer", version = '10')
+  ),
+  "Windows 7" = list(list(browser = "chrome", version = '34')
+                     , list(browser = "firefox", version = '29')
+                     , list(browser = "internet explorer", version = '10')
+  ),
+  "Linux" = list(list(browser = "chrome", version = '34')
+                 , list(browser = "firefox", version = '28')
+                 , list(browser = "opera", version = '12')
+  )
+)
 
-testsel <- test_env()
-with(testsel, {rsel.opt <- list(remoteServerAddr = ip, port = port, browserName = browser
-                               , version = version, platform = platform
-                               , extraCapabilities = list(username = user, accessKey = pass))
-               sauceTest <- TRUE
-               })
-testRes <- test_dir("./inst/tests", reporter = "Tap", filter = "api-example", env = testsel)
+out <- lapply(names(osBrowser), function(x){
+  platform <- x
+  lapply(osBrowser[[x]], platform = platform, FUN = function(y, platform){
+    rdBrowser <- y$browser
+    version <- y$version
+    testsel <- test_env()
+    testsel[['sauceTest']] <- TRUE
+    testsel[['rsel.opt']] <- list(remoteServerAddr = ip, port = port, browserName = rdBrowser
+                                  , version = version, platform = platform
+                                  , extraCapabilities = list(username = user, accessKey = pass, "selenium-version" = selVersion))
+    testRes <- test_dir(testDir, reporter = "Tap", filter = "api-example", env = testsel)
+    list(testsel[['rsel.opt']]$id, testRes)
+  })
+}) 
 
 if(!any(testRes$failed) && testsel[['sauceTest']]){
   # test passed rsel.opt should contain the jobid
@@ -30,6 +48,5 @@ if(!any(testRes$failed) && testsel[['sauceTest']]){
   res <- getURLContent(ip, customrequest = "PUT", httpheader = "Content-Type:text/json", postfields = qdata, isHTTP = FALSE)
   
 }
-  
 
-  
+
