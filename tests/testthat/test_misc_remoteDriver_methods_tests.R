a5d3f6c179c648ba594916068eea33014f39f370
johndharrison <johndharrison0@gmail.com>
2016-09-20 06:45:32 -0700

    Add tests on misc methods in remoteDriver class to TRAVIS

---
+++ tests/testthat/test_misc_remoteDriver_methods_tests.R
@@ -0,0 +1,53 @@
+context("misc_remoteDriver_methods_tests")
+init <- initFun()
+remDr <- init$remDr; rdBrowser <- init$rdBrowser; loadPage <- init$loadPage
+on.exit(remDr$closeall())
+
+test_that("canShowRemoteDriver", {
+  expect_identical(remDr$show()$browserName, rdBrowser)
+}
+)
+
+test_that("canShowErrorClass", {
+  expect_identical(remDr$showErrorClass()$status, 0L)
+}
+)
+
+test_that("canGetSessions", {
+  sessions <- remDr$getSessions()  
+  expect_equal(length(sessions), 1L)
+  expect_identical(session[[1]][["id"]], remDr$sessionid)
+}
+)
+
+test_that("canGetStatus", {
+  status <- remDr$getStatus()
+  expect_identical(names(status), c("build", "os", "java" ))
+}
+)
+
+test_that("canSetAsyncScriptTimeout", {
+  expect_silent(remDr$setAsyncScriptTimeout())
+}
+)
+
+test_that("canSetImplicitWaitTimeout", {
+  expect_silent(remDr$setImplicitWaitTimeout())
+}
+)
+
+test_that("canGetLogTypes", {
+  expect_gt(length(remDr$getLogTypes()), 0L)
+})
+
+test_that("canGetLog", {
+  logs <- remDr$getLogTypes()[[1]]  
+  expect_true(inherits(remDr$log(logs[1]), "list"))
+}
+)
+
+test_that("canGetPageSource", {
+  remDr$navigate(loadPage("simpleTest"))
+  source <- remDr$getPageSource()
+  expect_true(grepl("html", source[[1]]))
+})
