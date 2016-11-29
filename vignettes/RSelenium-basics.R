c06c8879bb0bb575de74f8a26f3e0abd82910339
johndharrison <johndharrison0@gmail.com>
2016-11-29 06:14:03 -0800

    Update docs

---
+++ vignettes/RSelenium-basics.R
@@ -0,0 +1,6 @@
+## ---- echo = FALSE, message = FALSE--------------------------------------
+library(knitr)
+library(RSelenium)
+opts_chunk$set(comment = "#>", error = TRUE, tidy = TRUE)
+
+
---
+++ vignettes/RSelenium-headless.R
@@ -0,0 +1,5 @@
+## ---- echo = FALSE, message = FALSE--------------------------------------
+library(knitr)
+library(RSelenium)
+opts_chunk$set(comment = "#>", error = TRUE, tidy = FALSE)
+
