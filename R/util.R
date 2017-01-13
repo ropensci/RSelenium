3cc65a386076f907f40c9386f3b69fe663ae93cf
johndharrison <johndharrison0@gmail.com>
2017-01-13 13:03:25 -0800

    checkForServer and startServer are now defunct.

--- R/util.R
+++ R/util.R
@@ -1,5 +1,7 @@
 #' Check for Server binary
 #' 
+#' Defunct. Please use \code{\link{rsDriver}}
+#' 
 #' \code{checkForServer}
 #' A utility function to check if the Selenium Server stanalone binary is 
 #'    present.
@@ -24,53 +26,16 @@
 checkForServer <- function (dir = NULL, update = FALSE, rename = TRUE
                             , beta = FALSE) 
 {
-  .Deprecated(package = "RSelenium", msg = "checkForServer is deprecated.
-Users in future can find the function in 
-file.path(find.package(\"RSelenium\"), \"example/serverUtils\").
-The sourcing/starting of a Selenium Server is a users responsiblity. 
-Options include manually starting a server see 
-vignette(\"RSelenium-basics\", package = \"RSelenium\")
-and running a docker container see 
-vignette(\"RSelenium-docker\", package = \"RSelenium\")")
-  selURL <- "http://selenium-release.storage.googleapis.com"
-  selXML <- xmlParse(paste0(selURL), "/?delimiter=")
-  selJAR <- 
-    xpathSApply(selXML, 
-                "//s:Key[contains(text(),'selenium-server-standalone')]", 
-                namespaces = 
-                  c(s = "http://doc.s3.amazonaws.com/2006-03-01"), 
-                xmlValue
-    )
-  
-  # get the most up-to-date jar
-  selJAR <- if(!beta){
-    grep("^.*-([0-9\\.]*)\\.jar$", selJAR, value = TRUE)
-  }else{
-    selJAR
-  }
-  
-  selJARdownload <- selJAR[order(gsub(".*-(.*).jar$", "\\1", selJAR), 
-                                 decreasing = TRUE)][1]
-  selDIR <- ifelse(is.null(dir), file.path(find.package("RSelenium"), 
-                                           "bin"), dir)
-  selFILE <- if(rename){
-    file.path(selDIR, "selenium-server-standalone.jar")
-  }else{
-    file.path(selDIR, gsub(".*(selenium-server-standalone.*)", 
-                           "\\1", selJARdownload)
-    )
-  }
-  
-  if (update || !file.exists(selFILE)) {
-    dir.create(selDIR, showWarnings=FALSE)
-    message("DOWNLOADING STANDALONE SELENIUM SERVER. THIS MAY TAKE SEVERAL 
-            MINUTES")
-    download.file(paste0( selURL, "/", selJARdownload), selFILE
-                  , mode = "wb")
-  }
+  .Defunct(rsDriver, "RSelenium",
+"checkForServer is now defunct. Users in future can find the function in 
+file.path(find.package(\"RSelenium\"), \"example/serverUtils\"). The
+recommended way to run a selenium server is via Docker. Alternatively
+see the RSelenium::rsDriver function.")
 }
 
 #' Start the standalone server.
+#' 
+#' #' Defunct. Please use \code{\link{rsDriver}}
 #' 
 #' \code{startServer}
 #' A utility function to start the standalone server. Return two functions 
@@ -112,91 +77,11 @@ vignette(\"RSelenium-docker\", package = \"RSelenium\")")
 startServer <- function (dir = NULL, args = NULL, javaargs = NULL, 
                          log = TRUE,  ...) 
 {
-  .Deprecated(package = "RSelenium", msg = "startServer is deprecated.
-Users in future can find the function in 
-file.path(find.package(\"RSelenium\"), \"example/serverUtils\").
-The sourcing/starting of a Selenium Server is a users responsiblity. 
-Options include manually starting a server see 
-vignette(\"RSelenium-basics\", package = \"RSelenium\")
-and running a docker container see  
-vignette(\"RSelenium-docker\", package = \"RSelenium\")")
-  selDIR <-  ifelse(is.null(dir), file.path(find.package("RSelenium"), 
-                                            "bin"), dir)
-  selFILE <- file.path(selDIR, "selenium-server-standalone.jar")
-  if (!file.exists(selFILE)) {
-    possFiles <- list.files(selDIR, "selenium-server-standalone")
-    if(length(possFiles) == 0){
-      stop("No Selenium Server binary exists. Run checkForServer or start 
-           server manually.")
-    }
-    # pick most recent driver
-    selFILE <- possFiles[order(gsub(".*-(.*).jar$", "\\1", possFiles), 
-                               decreasing = TRUE)][1]
-    selFILE <- file.path(selDIR, selFILE)
-  }
-  logFILE <- file.path(selDIR, "sellog.txt")
-  selArgs <- c(paste("-jar", shQuote(selFILE)))
-  if(log){
-    write("", logFILE)
-    selArgs <- c(selArgs, paste("-log", shQuote(logFILE)))
-  }
-  selArgs <- c(javaargs, selArgs, args)
-  userArgs <- list(...)
-  if (.Platform$OS.type == "unix") {
-    initArgs <- list(command = "java", args = selArgs, wait = FALSE, 
-                     stdout = FALSE, stderr = FALSE)
-  }
-  else {
-    initArgs <- list(command = "java",args = selArgs, wait = FALSE, 
-                     invisible = TRUE)
-  }
-  initArgs[names(userArgs)] <- userArgs 
-  do.call(system2, initArgs)
-  if (.Platform$OS.type == "windows"){
-    wmicOut <- tryCatch({
-      system2("wmic",
-              args = c("path win32_process get Caption,Processid,Commandline"
-                       , "/format:htable")
-              , stdout=TRUE, stderr=NULL)
-    }, error = function(e)e)
-    selPID <- if(inherits(wmicOut, "error")){
-      wmicArgs <- paste0(c("path win32_process where \"commandline like '%",
-                           selFILE, "%'\" get Processid"))
-      wmicOut <- system2("wmic", 
-                         args = wmicArgs
-                         , stdout = TRUE)
-      as.integer(gsub("\r", "", wmicOut[2]))
-    }else{
-      wmicOut <- readHTMLTable(htmlParse(wmicOut), header = TRUE, 
-                               stringsAsFactors = FALSE)[[1]]
-      wmicOut[["ProcessId"]] <- as.integer(wmicOut[["ProcessId"]])
-      idx <- grepl(selFILE, wmicOut$CommandLine)
-      if(!any(idx)) stop("Selenium binary error: Unable to start Selenium 
-                         binary. Check if java is installed.")
-      wmicOut[idx,"ProcessId"]
-    }
-  }else{
-    if(Sys.info()["sysname"] == "Darwin"){
-      sPids <- system('ps -Ao"pid"', intern = TRUE)
-      sArgs <- system('ps -Ao"args"', intern = TRUE)
-    }else{
-      sPids <- system('ps -Ao"%p"', intern = TRUE)
-      sArgs <- system('ps -Ao"%a"', intern = TRUE)
-    }
-    idx <- grepl(selFILE, sArgs)
-    if(!any(idx)) stop("Selenium binary error: Unable to start Selenium 
-                       binary. Check if java is installed.")
-    selPID <- as.integer(sPids[idx])
-  }
-  
-  list(
-    stop = function(){
-      tools::pskill(selPID)
-    },
-    getPID = function(){
-      return(selPID)
-    }
-  )
+  .Defunct(rsDriver, "RSelenium",
+"startServer is now defunct. Users in future can find the function in 
+file.path(find.package(\"RSelenium\"), \"example/serverUtils\"). The
+recommended way to run a selenium server is via Docker. Alternatively
+see the RSelenium::rsDriver function.")
 }
 
 #' Get Firefox profile.
@@ -470,6 +355,9 @@ makeFirefoxProfile <- function(opts){
 
 
 testWebElement <- function(x, remDr){
+  if(inherits(remDr, "webElement")){
+    remDr <- remDr$export("remoteDriver")
+  }
   replaceWE <- function(x, remDr){
     if(identical(names(x), "ELEMENT")){
       webElement$new(as.character(x))$import(remDr)
