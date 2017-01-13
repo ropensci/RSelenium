aed68f4f55b5b3df437e3b39365439846cae6fd9
John Harrison <johndharrison0@gmail.com>
2017-01-13 19:50:33 +0000

    Add rsDriver function

--- R/rsDriver.R
+++ R/rsDriver.R
@@ -1,28 +1,96 @@
-#' Title
+#' Start a selenium server and browser
 #'
-#' @param port 
-#' @param verbose 
-#' @param browser 
-#' @param ... 
+#' @param port Port to run on
+#' @param browser Which browser to start
+#' @param version what version of Selenium Server to run. Default = "latest"
+#'     which runs the most recent version. To see other version currently
+#'     sourced run binman::list_versions("seleniumserver")
+#' @param chromever what version of Chrome driver to run. Default = "latest"
+#'     which runs the most recent version. To see other version currently
+#'     sourced run binman::list_versions("chromedriver"), A value of NULL
+#'     excludes adding the chrome browser to Selenium Server.
+#' @param geckover what version of Gecko driver to run. Default = "latest"
+#'     which runs the most recent version. To see other version currently
+#'     sourced run binman::list_versions("geckodriver"), A value of NULL
+#'     excludes adding the firefox browser to Selenium Server.
+#' @param phantomver what version of PhantomJS to run. Default = "latest"
+#'     which runs the most recent version. To see other version currently
+#'     sourced run binman::list_versions("phantomjs"), A value of NULL
+#'     excludes adding the PhantomJS headless browser to Selenium Server.
+#' @param iedrver what version of IEDriverServer to run. Default = "latest"
+#'     which runs the most recent version. To see other version currently
+#'     sourced run binman::list_versions("iedriverserver"), A value of NULL
+#'     excludes adding the internet explorer browser to Selenium Server.
+#'     NOTE this functionality is Windows OS only.
+#' @param verbose If TRUE, include status messages (if any)
+#' @param ... Additional arguments to pass to \code{\link{remoteDriver}}
 #'
-#' @return
+#' @return A list containing a server and a client. The server is the object
+#' returned by \code{\link[wdman]{selenium}} and the client is an object of class
+#' \code{\link{remoteDriver}}
+#' @details This function is a wrapper around \code{\link[wdman]{selenium}}.
+#'     It provides a "shim" for the current issue running firefox on 
+#'     Windows. For a more detailed set of functions for running binaries
+#'     relating to the Selenium/webdriver project see the 
+#'     \code{\link[wdman]{wdman}} package.
 #' @export
 #' @importFrom wdman selenium
 #'
 #' @examples
+#' # start a chrome browser
+#' rD <- rsDriver()
 
-rsDriver <- function(port = 4567L, verbose = TRUE, 
+rsDriver <- function(port = 4567L,
                      browser = c("chrome", "firefox", "phantomjs", 
-                                 "internet explorer"), ...){
-  selServ <- wdman::selenium(port = port, verbose = verbose, ...)
+                                 "internet explorer"),
+                     version = "latest",
+                     chromever = "latest",
+                     geckover = "latest",
+                     iedrver = NULL,
+                     phantomver = "latest", 
+                     verbose = TRUE, ...){
+  selServ <- wdman::selenium(port = port, verbose = verbose)
   browser <- match.arg(browser)
-  # shim for blocking pipe windows issues
-  if(identical(binman:::get_os(), "windows")){
-    remDr <- httr::with_config(
-      httr::timeout(3000), 
-      {remoteDriver(browserName = browser, port = 4567L)}
+  remDr <- remoteDriver(browserName = browser, port = port, ...)
+  # shim for blocking pipe issue on windows and firefox
+  if(identical(binman:::get_os(), "win")){
+    res <- tryCatch(
+      httr::with_config(
+        httr::timeout(3), 
+        remDr$open(silent = !verbose)
+      ),
+      error = function(e){e}
     )
+    if(inherits(res, "error")){
+      if(!grepl("Timeout was reached", res[["message"]])){
+        selServ$stop()
+        stop(res[["message"]])
+      }else{
+        oldSessions <- length(remDr$getSessions())
+        chk <- NA_character_
+        while(!identical(chk, character())){
+          chk <- selServ$error(timeout = 1000)
+        }
+        count <- 0L
+        while(length(sessions <- remDr$getSessions()) <= oldSessions){
+          Sys.sleep(1)
+          count <- count + 1L
+          if(count > 4L){
+            selServ$stop()
+            stop("Could not start new browser")
+          }
+        }
+        sessions <- remDr$getSessions()
+        remDr$sessionInfo <- sessions[[length(sessions)]]
+      }
+    }
   }else{
-    remDr <- remoteDriver(browserName = browser, port = 4567L)
+    if(identical(browser, "internet explorer")){
+      selServ$stop()
+      stop("Internet Explorer is only available on Windows.")
+    }
+    remDr <- remoteDriver(browserName = browser, port = port)
+    remDr$open(silent = !verbose)
   }
+  list(server = selServ, client = remDr)
 }
