f7f952c54237e563ae13c604065b868ca4134127
johndharrison <johndharrison0@gmail.com>
2017-01-20 05:43:56 -0800

    Remoce windows/firefox shim. Add check on server status and give warning if it cannot be determined.

--- R/rsDriver.R
+++ R/rsDriver.R
@@ -76,45 +76,25 @@ rsDriver <- function(port = 4567L,
                              check = TRUE)
   browser <- match.arg(browser)
   remDr <- remoteDriver(browserName = browser, port = port, ...)
-  # shim for blocking pipe issue on windows and firefox
-  if(identical(binman:::get_os(), "win") && identical(browser, "firefox")){
-    res <- tryCatch(
-      httr::with_config(
-        httr::timeout(4), 
-        remDr$open(silent = !verbose)
-      ),
-      error = function(e){e}
-    )
-    if(inherits(res, "error")){
-      if(!grepl("Timeout was reached", res[["message"]])){
-        selServ$stop()
-        stop(res[["message"]])
-      }else{
-        chk <- NA_character_
-        while(!identical(chk, character())){
-          chk <- selServ$error(timeout = 1000)
-        }
-        count <- 0L
-        while(length(sessions <- remDr$getSessions()) == 0L){
-          Sys.sleep(1)
-          count <- count + 1L
-          if(count > 4L){
-            selServ$stop()
-            stop("Could not start new browser")
-          }
-        }
-        sessions <- remDr$getSessions()
-        remDr$sessionInfo <- sessions[[length(sessions)]]
-      }
-    }
-  }else{
-    if(identical(browser, "internet explorer")){
-      selServ$stop()
-      stop("Internet Explorer is only available on Windows.")
-    }
-    remDr <- remoteDriver(browserName = browser, port = port, ...)
-    remDr$open(silent = !verbose)
+  if(identical(browser, "internet explorer")){
+    selServ$stop()
+    stop("Internet Explorer is only available on Windows.")
   }
+  remDr <- remoteDriver(browserName = browser, port = port, ...)
+  # check server status
+  count <- 0L
+  while(inherits(res <- tryCatch({remDr$getStatus() }, 
+                                 error = function(e){e}),
+                 "error")){
+    Sys.sleep(1)
+    count <- count + 1L
+    if(count > 5L){
+      warning("Could not determine server status.")
+      break
+    }
+  }
+  remDr$open(silent = !verbose)
+  
   csEnv <- new.env()
   csEnv[["server"]] <- selServ
   csEnv[["client"]] <- remDr
