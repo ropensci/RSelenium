b10613c663f780d86b4f106174703ff085697fdb
J. Kim <jkim2345@fredhutch.org>
2018-09-05 18:05:54 -0700

    Create package R file for documentation

---
+++ R/RSelenium.R
@@ -0,0 +1,12 @@
+#' @title An R client for Selenium Remote Webdriver
+#'
+#' @description These are R bindings for the WebDriver API in Selenium 2. They use 
+#' the JsonWireProtocol defined at 
+#' http://code.google.com/p/selenium/wiki/JsonWireProtocol to communicate with 
+#' a Selenium RemoteWebDriver Server.
+#'
+#' @name RSelenium-package
+#' @aliases RSelenium
+#' @author John Harrison
+#' @references http://seleniumhq.org/projects/webdriver/
+NULL
